3866a45f759b8b6d5637430d7c78b1ddc13b4f97
Philip Andersson <philip.andersson.1991@hotmail.com>
2023-03-21 09:22:44 +0100

    Initial Commit

---
+++ SpeedtyperUITests/SpeedtyperUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SpeedtyperUITestsLaunchTests.swift
+//  SpeedtyperUITests
+//
+//  Created by Philip Andersson on 2023-03-21.
+//
+
+import XCTest
+
+final class SpeedtyperUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
